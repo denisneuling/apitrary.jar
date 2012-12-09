8b844746b5e85a6cc7140a49c948e1a96910deca
Denis Neuling <denisneuling@gmail.com>
2012-12-09 16:01:39 +0100

    Add Copyright

--- apitrary-orm/apitrary-orm-core/src/main/java/com/apitrary/orm/core/AbstractApitraryDaoSupport.java
+++ apitrary-orm/apitrary-orm-core/src/main/java/com/apitrary/orm/core/AbstractApitraryDaoSupport.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Denis Neuling 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package com.apitrary.orm.core;
 
 import java.util.List;
@@ -10,6 +25,10 @@ import com.apitrary.orm.core.scheme.SchemeCache;
 import com.apitrary.orm.annotations.Entity;
 import com.apitrary.orm.annotations.Id;
 
+/**
+ * 
+ * @author Denis Neuling (denisneuling@gmail.com)
+ */
 public abstract class AbstractApitraryDaoSupport {
 
 	protected SchemeCache schemeCache = new SchemeCache();
--- apitrary-orm/apitrary-orm-core/src/main/java/com/apitrary/orm/core/constraint/IdResolver.java
+++
@@ -1,8 +0,0 @@
-package com.apitrary.orm.core.constraint;
-
-public class IdResolver {
-
-	public <T> String resolveId(T entity) throws Exception{
-		return null;
-	}
-}
